d5dbdce23ce6fab6040e515cde5c5ed221523e6f
varrtix <tiamo_nana@outlook.com>
2019-11-22 23:09:42 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -4,18 +4,17 @@
 # Author: varrtix<tiamo_nana@outlook.com>
 # Intro: https://github.com/varrtix/varsc
 
-# Guess => Portability
-# PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
-# export PATH
+# script version
+readonly VERSION="1.0"
+readonly AUTHORS=("varrtix<tiamo_nana@outlook.com>")
+readonly MACHINE=$(uname -s)
 
-version="1.0"
-authors=("varrtix<tiamo_nana@outlook.com>")
-
-red='\033[31m'
-green='\033[32m'
-yellow='\033[33m'
-plain='\033[0m'
-bold='\033[1m'
+# terminal font style
+readonly RED_FS='\033[31m'
+readonly GREEN_FS='\033[32m'
+readonly YELLOW_FS='\033[33m'
+readonly PLAIN_FS='\033[0m'
+readonly BOLD_FS='\033[1m'
 
 ###############################################################################
 # Output the error msg and when it occurred to stderr.
@@ -26,22 +25,77 @@ bold='\033[1m'
 # Returns:
 #  None
 # Example:
-#  err "string"
+#  err "error_msg"
 ###############################################################################
 err() {
-  echo -e "[$(date +'%Y-%m-%dT%H:%M:%S%:z')]${red}Error:${plain} ${bold}$@${plain}" >&2
+  echo -e "[$(date +'%Y-%m-%dT%H:%M:%S%:z')]${RED_FS}Error:${PLAIN_FS} ${BOLD_FS}$@${PLAIN_FS}" >&2
 }
 
-clear
-cat <<EOF
+###############################################################################
+# initialize menu
+# Globals:
+#  None
+# Arguments:
+#  None
+# Returns:
+#  None
+###############################################################################
+init() {
+  clear
+  cat <<EOF
 +---------------------------------------------------------+
-|                       Delay tools                       |
+                        Delay tools
 +---------------------------------------------------------+
-|  Auto install all the required settings for new system  |
+   Auto install all the required settings for new system
 +---------------------------------------------------------+
-|  Authors: ${authors[0]}               |
+   Authors: ${AUTHORS[0]}
++---------------------------------------------------------+
+   Version: ${VERSION}
 +---------------------------------------------------------+
 EOF
 
-# func_intro(){
+}
+
+###############################################################################
+# Check system name
+# Globals:
+#  None
+# Arguments:
+#  None
+# Returns:
+#  None
+###############################################################################
+# check_sys() {
 # }
+
+###############################################################################
+# Main entrance for the script.
+# Globals:
+#  None
+# Arguments:
+#  None
+# Returns:
+#  None
+###############################################################################
+main() {
+# Guess => Portability
+# PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+# export PATH
+  [[ $EUID -ne 0 ]] && err "Make sure run the script as superuser." && exit 1
+  # readonly machine=$(check_machine)
+  echo ${MACHINE}
+  # check_sys
+}
+
+main "$@"
+###############################################################################
+# string
+# Globals:
+#  None
+# Arguments:
+#  None
+# Returns:
+#  None
+# Example:
+#  None
+###############################################################################
