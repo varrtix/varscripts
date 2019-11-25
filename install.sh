b0bfb37320ffba3aaadabab3c84d84086692d6eb
varrtix <tiamo_nana@outlook.com>
2019-11-25 17:15:18 +0800

    add interface check && package manager prefix

--- install.sh
+++ install.sh
@@ -7,8 +7,12 @@
 # script version
 readonly VERSION="1.0"
 readonly AUTHORS=("varrtix<tiamo_nana@outlook.com>")
-local SYSTEM=''
-local RELEASE=''
+local MODE
+local SYSTEM
+local RELEASE
+local REMOTE_DIR
+# The tool name of package manager in system.
+local PM
 
 # terminal font style
 readonly RED_FS='\033[31m'
@@ -50,14 +54,12 @@ check_sys() {
       ;;
     Linux)
       if [[ -s /etc/os-release ]]; then
-          SYSTEM=$(cat /etc/os-release \
-		       | grep -E "^NAME=+" \
-		       | awk -F "\"" '{print $2}' \
-		       | awk '{print $1}')
-	  RELEASE=$(cat /etc/os-release \
-			| grep -E "^VERSION=+" \
-			| awk -F "\"" '{print $2}' \
-			| awk '{print $1}')
+        SYSTEM=$(cat /etc/os-release \
+					| grep -E "^NAME=+" \
+					| awk '{ gsub(/="|=/, " "); print $2}')
+	      RELEASE=$(cat /etc/os-release \
+			    | grep -E "^VERSION=+" \
+					| awk '{ gsub(/="/, " "); print $2}')
       fi
       ;;
     *)
@@ -66,13 +68,57 @@ check_sys() {
       return 1
       ;;
   esac
+	readonly SYSTEM
+	readonly RELEASE
   return 0
+}
+
+###############################################################################
+# Set package manager prefix
+# Globals:
+#  SYSTEM PM(yum, dnf, apt...etc.)
+# Arguments:
+#  SYSTEM
+# Returns:
+#  Success return 0, Failed return 1
+###############################################################################
+pm_prefix() {
+	case $1 in
+		'macOS') PM='brew';;
+		'CentOS' | 'Redhat') [[ ${RELEASE} -gt 7 ]] && PM='dnf' || PM='yum';;
+		'Fedora') [[ ${RELEASE} -gt 21 ]] && PM='dnf' || PM='yum';;
+		'Ubuntu') PM='apt';;
+		*) PM='unknown'; return 1;;
+	esac
+	readonly PM
+	return 0
+}
+
+###############################################################################
+# Check the interface mode of the system.
+# Globals:
+#  SYSTEM MODE
+# Arguments:
+#  SYSTEM
+# Returns:
+#  Success return 0, Failed return 1
+###############################################################################
+check_interface() {
+  case ${SYSTEM} in
+		'macOS') MODE='GUI';;
+    'CentOS')
+			MODE='CLI'
+    ;;
+	  *) MODE='Unknown'; return 1;;
+  esac
+  readonly MODE
+	return 0
 }
 
 ###############################################################################
 # initialize menu view
 # Globals:
-#  None
+#  SYSTEM
 # Arguments:
 #  None
 # Returns:
@@ -89,7 +135,7 @@ menu_init() {
 +---------------------------------------------------------+
   Authors: ${AUTHORS[0]}
 +---------------------------------------------------------+
-  System: ${SYSTEM} ${RELEASE}
+  System: ${SYSTEM} ${RELEASE} | Mode: ${MODE}
 +---------------------------------------------------------+
   Version: ${VERSION}
 +---------------------------------------------------------+
@@ -111,16 +157,9 @@ EOF
 macos_menu() {
   cat <<EOF
 Functions:
-[1] Create backup
-EOF
-}
-
-centos_menu() {
-  cat<<EOF
-Functions:
-[1] Replace repo
-[2] Restore repo
-[3] Install vim
+[1] Set Shell Configuration
+[2] Install Homebrew
+[3] Install Vim
 [4] Install Emacs
 
 EOF
@@ -132,8 +171,32 @@ EOF
     2);;
     3);;
     4);;
+  esac
+
+}
+
+centos_menu() {
+  cat<<EOF
+Functions:
+[1] Replace repo
+[2] Restore repo
+[3] Install vim
+[4] Install Emacs
+[5] Set Shell Configuration
+
+EOF
+  local opt
+  read -p "Please select a number of options: " opt
+  readonly opt
+  case ${opt} in
+    1);;
+    2);;
+    3);;
+    4);;
+    5);;
     *);;
   esac
+
 }
 
 ###############################################################################
@@ -151,7 +214,11 @@ main() {
 # export PATH
   err $EUID "Make sure run the script as superuser."
   check_sys $(uname -s)
-  err $? "Unknown system."
+  err $? "System: ${SYSTEM}"
+	check_interface ${SYSTEM}
+	err $? "Mode: ${MODE}"
+	pm_prefix ${SYSTEM}
+	err $? "Cannot set package manager Prefix: ${PM}"
   menu_init SYSTEM RELEASE
 }
 
